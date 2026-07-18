d01e3a13c80ea4695b2ca6f9e0daa1bd41b54aa6
Prashant Thorat <prashant.workemail0@gmail.com>
2026-07-18 20:44:55 +0530

    Hero_Home and Awards Section added in home

--- src/landing_page/home/Hero.js
+++ src/landing_page/home/Hero.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from "react";
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-     );
+  return (
+    <div className="container p-5 mb-5">
+      <div className="row text-center">
+        <img
+          src="media/images/homeHero.png"
+          alt="homeHero Image"
+          className="mb-5"
+        />
+        <h1 className="mt-5 ">Invest in everything</h1>
+        <p>
+          Online Platform to invest in stocks, derivatives, mutual funds, and
+          more
+        </p>
+        <button
+          className="p-2 btn btn-primary fs-5 mb-5 "
+          style={{ width: "15%", margin: "0 auto" }}
+        >
+          Sign up for free
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
