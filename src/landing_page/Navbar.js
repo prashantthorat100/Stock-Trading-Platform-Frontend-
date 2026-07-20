6f62389ae50fcd1b824dba60077c6c773fda90d0
Prashant Thorat <prashant.workemail0@gmail.com>
2026-07-21 01:13:08 +0530

    Home page completed

--- src/landing_page/Navbar.js
+++ src/landing_page/Navbar.js
@@ -1,8 +1,58 @@
-import React from 'react'
+import React from "react";
 function Navbar() {
-    return ( 
-        <h1>Navbar</h1>
-     );
+  return (
+    <nav
+      class="navbar navbar-expand-lg border-bottom"
+      style={{ background: "#fff" }}
+    >
+      <div class="container p-2">
+        <a class="navbar-brand" href="#">
+          <img
+            style={{ width: "25%", marginLeft: "40px" }}
+            alt="logo"
+            src="media/images/logo.svg"
+          />
+        </a>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <form class="d-flex" role="search">
+            <ul class="navbar-nav mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Signup
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Products
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Support
+                </a>
+              </li>
+              <li class="">
+                <a class="nav-link active" href="#">
+                  <i class="fa-solid fa-bars"></i>
+                </a>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
--- src/landing_page/OpenAccount.js
+++ src/landing_page/OpenAccount.js
@@ -1,8 +1,22 @@
 import React from 'react'
 function OpenAccount() {
-    return ( 
-        <h1>OpenAccount</h1>
-     );
+    return (
+      <div className="container p-5 mb-1">
+        <div className="row text-center">
+          <h1 className="mb-4">Open a Zerodha account</h1>
+          <p className='mb-5'>
+            Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and
+            F&O trades.
+          </p>
+          <button
+            className="p-2 btn btn-primary fs-5 mb-5 "
+            style={{ width: "15%", margin: "0 auto" }}
+          >
+            Sign up for free
+          </button>
+        </div>
+      </div>
+    );
 }
 
 export default OpenAccount;
