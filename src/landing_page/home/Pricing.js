2567b957d2c4f87a647fec5be114122bc0c3d807
Prashant Thorat <prashant.workemail0@gmail.com>
2026-07-20 22:41:56 +0530

    pricing section in homepage added

--- src/landing_page/home/Pricing.js
+++ src/landing_page/home/Pricing.js
@@ -1,8 +1,46 @@
 import React from 'react'
 function Pricing() {
-    return ( 
-        <h1>Pricing</h1>
-     );
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <h1 className="mb-5">Unbeatable Pricing</h1>
+            <p className="mb-2">
+              We pioneered the concept of discount broking and price
+              transparency in India. Flat fees and no hidden charges.
+            </p>
+            <a href="" style={{ textDecoration: "none" }}>
+              See Pricing <i class="fas fa-arrow-right"></i>
+            </a>
+          </div>
+          <div className="col-2"></div>
+          <div className="col-6">
+            <div className="row  text-center ">
+              <div className="col-6 border p-3 ">
+                <h1 className='mb-3'>
+                  <i
+                    class="fa-solid fa-indian-rupee-sign"
+                    style={{ fontSize: "30px" }}
+                  ></i>
+                  0
+                </h1>
+                <p>Free equity delivery and<br/> direct mutual funds</p>
+              </div>
+              <div className="col-6 border p-3">
+                <h1 className='mb-3'>
+                  <i
+                    class="fa-solid fa-indian-rupee-sign"
+                    style={{ fontSize: "30px" }}
+                  ></i>
+                  20
+                </h1>
+                <p>Intraday and F&O</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default Pricing;
