5f205452014cb517128ff7ed768bf192f826e82c
Prashant Thorat <prashant.workemail0@gmail.com>
2026-07-23 21:28:18 +0530

    product, support,pricing

--- src/landing_page/pricing/Hero.js
+++ src/landing_page/pricing/Hero.js
@@ -1,8 +1,55 @@
-import React from 'react'
+import React from "react";
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-     );
+  return (
+    <div className="container border-bottom mb-5 mt-5">
+      <div className="row  p-5 text-center ">
+        <h1 className="fs-3">Charges</h1>
+        <p className="fs-4 text-muted">List of all charges and taxes</p>
+      </div>
+
+      <div className="row p-5">
+        <div className="col-4  text-center">
+          <img
+            style={{ width: "250px" }}
+            src="media\images\pricing0.svg"
+            alt="0"
+          ></img>
+          <h1 className="fs-3 ">Free equity delivery</h1>
+          <p className="text-muted mt-4">
+            All equity delivery investments (NSE, BSE),
+            <br /> are absolutely free — ₹ 0 brokerage.
+          </p>
+        </div>
+        <div className="col-4  text-center">
+          <img
+            style={{ width: "250px" }}
+            src="media\images\intradayTrades.svg"
+            alt="20"
+          ></img>
+          <h1 className="fs-3 ">Intraday and F&O trades</h1>
+          <p className="text-muted mt-4">
+            Flat ₹ 20 or 0.03% (whichever is lower) per
+            <br /> executed order on intraday trades across
+            <br /> equity, currency, and commodity trades. Flat
+            <br /> ₹20 on all option trades.
+          </p>
+        </div>
+        <div className="col-4  text-center">
+          <img
+            style={{ width: "250px" }}
+            src="media\images\pricing0.svg"
+            alt="0"
+          ></img>
+          <h1 className="fs-3 ">Free direct MF</h1>
+          <p className="text-muted mt-4">
+            All direct mutual fund investments are
+            <br /> absolutely free — ₹ 0 commissions & DP
+            <br /> charges.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- src/landing_page/products/Universe.js
+++ src/landing_page/products/Universe.js
@@ -1,8 +1,96 @@
-import React from 'react'
-function Universe() {
-    return ( 
-        <h1>Universe</h1>
-     );
+import React from "react";
+function Universe({ imageURL, description }) {
+  return (
+    <div className="container mb-5">
+      <div className="row text-center mt-5 mb-5 p-5 ">
+        <div className="mb-5">
+          <h1 className=" fs-4 mb-4">The Zerodha Universe</h1>
+          <p>
+            Extend your trading and investment experience even further with our
+            partner platforms
+          </p>
+        </div>
+
+        <div className="row">
+          <div className="col-4 p-3">
+            <img
+              style={{ width: "60%", marginBottom: "25px" }}
+              src="media\images\zerodhaFundhouse.png"
+              alt="zerodhaFundhouse"
+            />
+            <p className="text-small text-muted">
+              Our asset management venture <br /> that is creating simple and
+              transparent index <br /> funds to help you save for your goals
+            </p>
+          </div>
+          <div className="col-4 p-3">
+            <img
+              style={{ width: "60%", marginBottom: "25px" }}
+              src="media\images\sensibullLogo.svg"
+              alt="sensibullLogo"
+            />
+            <p className="text-small text-muted">
+              Options trading platform that lets you
+              <br /> create strategies, analyze positions, and examine
+              <br /> data points like open interest, FII/DII, and more.
+            </p>
+          </div>
+          <div className="col-4 p-3">
+            <img
+              style={{ width: "40%", marginBottom: "25px" }}
+              src="media\images\tijori.svg"
+              alt="tijorilogo"
+            />
+            <p className="text-small text-muted">
+              Investment research platform <br /> that offers detailed insights
+              on stocks,
+              <br /> sectors, supply chains, and more.
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-4 p-3">
+            <img
+              style={{ width: "40%" }}
+              src="media\images\streak-logo.png"
+              alt="streaklogo"
+            />
+            <p className="text-small text-muted mt-4">
+              Systematic trading platform
+              <br /> that allows you to create and backtest
+              <br /> strategies without coding.
+            </p>
+          </div>
+          <div className="col-4 p-3">
+            <img src="media\images\smallcaseLogo.png" alt="smallcaseLogo" />
+            <p className="text-small text-muted mt-4">
+              Thematic investing platform
+              <br /> that helps you invest in diversified
+              <br /> baskets of stocks on ETFs.
+            </p>
+          </div>
+          <div className="col-4 p-3">
+            <img
+              style={{ width: "40%", marginBottom: "25px" }}
+              src="media\images\dittoLogo.png"
+              alt="dittoLogo"
+            />
+            <p className="text-small text-muted">
+              Personalized advice on life
+              <br /> and health insurance. No spam
+              <br /> and no mis-selling.
+            </p>
+          </div>
+        </div>
+        <button
+          className="p-2 btn btn-primary fs-5 mt-5 "
+          style={{ width: "15%", margin: "0 auto" }}
+        >
+          Sign up for free
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Universe;
